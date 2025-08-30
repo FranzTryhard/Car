0378a0d438bc715a314f23e15214b380acbb0e38
Francis Surban <frncs4class@gmail.com>
2025-08-30 11:54:46 +0800

    Initial Laravel project

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+
+public function boot(): void{// Load API routesRoute::middleware('api')->prefix('api')->group(base_path('routes/api.php'));
+
+        // Load Web routes
+            Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+    }
+}
